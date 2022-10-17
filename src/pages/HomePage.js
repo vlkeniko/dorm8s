966236d7abbee24c96e521f3132a78fa5afd0369
Eniko Valko <eaaenva@students.eaaa.dk>
2022-10-17 11:10:00 +0200

    not working thing

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,20 +3,54 @@ import Balance from "../components/Balance";
 import Calendar from "../components/Calendar";
 import Nav from "../components/Nav";
 import WM from "../components/WM";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import PostCard from "../components/PostCard";
 
 export default function HomePage() {
+  const params = useParams();
+  const currentuser = JSON.parse(params.user);
 
+  const [wms, setWms] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  async function getWms() {
+    const url = "https://dorm8s-default-rtdb.europe-west1.firebasedatabase.app/WachingMachines";
+    const response = await fetch(url);
+    const data = await response.json();
+    setWms(data.wms);
+  }
+
+  async function getUsers() {
+    const url = "https://dorm8s-default-rtdb.europe-west1.firebasedatabase.app/Users";
+    const response = await fetch(url);
+    const data = await response.json();
+    setUsers(data.users);
+  }
+
+  useEffect(() => {
+    getWms();
+    getUsers();
+  }, []);
 
   return (
     <>
+      <article>
+        <h1>Washing overview</h1>
+        <p>You are logged in as {currentuser.username}</p>
+        <div style={{ display: "flex", flexDirection: "column", width: "70%" }}>
+          {wms.map((wms) => (
+            <PostCard key={wms.wmid} wms={wms} users={users} />
+          ))}
+        </div>
+      </article>
+
       <div>HomePage</div>
       <Balance />
       <Calendar />
       <WM />
       <Nav />
-     
-      </>
-  )
-
-   
+    </>
+  );
+ 
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,97 @@
-import React from 'react'
-import Login from '../components/Login'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function LoginPage() {
+// 📁 pages/HomePage.js
+export default function HomePage() {
+  // Defines initial states of data values
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+
+  // Fetch is run to get list of users from
+  // users.json file in public folder
+  const getUsers = () => {
+    fetch("https://dorm8s-default-rtdb.europe-west1.firebasedatabase.app/Users")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (myJson) {
+        setUsers(myJson);
+      });
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  // When the form is submitted, an object is created (formData)...
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const formData = {
+      name: username,
+      password: password,
+    };
+
+    // Check to see if all fields were filled. If not, show an
+    // error message. If everything is good, loop though the
+    // list of users to find a match. If match, navigate to wash
+    // overview and transfer user information as props.
+    const validForm = formData.username && formData.password;
+    if (validForm) {
+      for (const user of users.users) {
+        if (
+          formData.username === user.username &&
+          formData.password === user.password
+        ) {
+          console.log("bingo");
+          const currentuser = {
+            id: user.id,
+            name: user.username,
+          };
+          navigate(`wash/${JSON.stringify(currentuser)}`);
+        }
+      }
+    } else {
+      setErrorMessage("Please, fill in all fields.");
+    }
+  }
+
   return (
     <>
-      <div>LoginPage</div>
-    <Login/>
+      <form onSubmit={handleSubmit}>
+        <fieldset style={{ width: "250px" }}>
+          <legend>Log in</legend>
+          <label>
+            Username{" "}
+            <input
+              type="text"
+              name="username"
+              placeholder="Type in username"
+              onChange={(e) => setUsername(e.target.value)}
+            ></input>
+          </label>
+          <label>
+            Password&nbsp;{" "}
+            <input
+              type="password"
+              name="password"
+              placeholder="Type in password"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+            <br></br>
+            <br></br>
+            <p>{errorMessage}</p>
+            <button style={{ float: "right" }}>Login</button>
+            <button type="reset" style={{ float: "right" }}>
+              Reset
+            </button>
+          </label>
+        </fieldset>
+      </form>
+      <p>Her: Nina, hello123</p>
+      <p>Him: Morten, hi123</p>
     </>
-  
-  )
+  );
 }
