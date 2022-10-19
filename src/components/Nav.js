be765beca76bc7962fa3139de9c59c4d0d0a4b2f
Eniko Valko <eaaenva@students.eaaa.dk>
2022-10-19 13:37:03 +0200

    added icons to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,57 @@
 // This documment shows the menuitems(links) in the app's main navigation menu
 // Implemented by app.js.
 import { NavLink } from "react-router-dom";
-import Home from "../pictures/home.png"
+import Home from "../pictures/home.png";
+import Kitchen from "../pictures/cutlery.png";
+import Laundry from "../pictures/laundry.png";
+import Finances from "../pictures/finances.png";
+import Profile from "../pictures/profile.png";
 
 export default function Nav() {
   return (
     <>
-        <nav>
-       
-      <NavLink className="navlink" to="/" >
-      Home</NavLink>
-      <NavLink className="navlink" to="/kitchen" >
-      Kitchen</NavLink>
-      <NavLink className="navlink" to="/laundry" >
-      Laundry</NavLink>
-      <NavLink className="navlink" to="/finances" >
-      Finances</NavLink>
-      <NavLink className="navlink" to="/login">
-      Log in</NavLink>
-    </nav>
+      <nav>
+        <div className="navlink-container">
+          <NavLink className="navlink" to="/" >
+            <img src={Home} alt="homeicon" className="nav-icon"/>
+            Home
+          </NavLink>
+        </div>
 
-<NavLink className="navlink navlink-profile" to="/profile" >
-Profile</NavLink>
+        <div className="navlink-container">
+          <NavLink className="navlink" to="/kitchen">
+            <img src={Kitchen} alt="homeicon" className="nav-icon"/>
+            Kitchen
+          </NavLink>
+        </div>
+
+        <div className="navlink-container">
+          <NavLink className="navlink" to="/laundry">
+            <img src={Laundry} alt="homeicon" className="nav-icon"/>
+            Laundry
+          </NavLink>
+        </div>
+
+        <div className="navlink-container">
+          <NavLink className="navlink" to="/finances">
+            <img src={Finances} alt="homeicon" className="nav-icon"/>
+            Finances
+          </NavLink>
+        </div>
+
+        <div className="navlink-container">
+          <NavLink className="navlink" to="/login">
+            <img src={Home} alt="homeicon" className="nav-icon"/>
+            Log in
+          </NavLink>
+        </div>
+      </nav>
+      <div className="navlink-container">
+        <NavLink className="navlink navlink-profile" to="/profile">
+          <img src={Profile} alt="homeicon" className="nav-icon"/>
+          Profile
+        </NavLink>
+      </div>
     </>
-
   );
 }
