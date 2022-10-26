af1d8c0d6ddf79f4720227d0a645652c5adaf30b
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-10-26 12:53:44 +0200

    Kitchen items data now comes from database

--- src/components/Kitchen.js
+++ src/components/Kitchen.js
@@ -1,12 +1,36 @@
-import React from "react";
+import { useEffect, useState, React } from "react";
+import { endpoint, getFromEndpoint } from '../utils/database_functions.js';
 import Finances from "../pictures/finances.png";
+import KitchenItemCard from "./KitchenItemCard.js";
 
 export default function Kitchen() {
+
+  const [items, setItems] = useState([]);
+ 
+  /* Getting items data */
+  async function getItems() {
+    const url = `${endpoint}/Items.json`;
+    let result = await getFromEndpoint(url);
+    setItems(result);
+  }
+
+  useEffect(() => {
+    getItems();
+  }, []);
+
+  
+
   return (
     <div className="kitchen-cards">
       <h2>To buy</h2>
       <div className="card-container">
-        <div className="to-buy-card">
+        {items.map((item, i) => {
+          // Return the element. Also pass key
+          return <KitchenItemCard key={item.id} itemname={item.name} alttext={item.name} type="to-buy-card" imgsrc={Finances} />
+          /* return <PostCard key={wm.id} wm={wm} users={users} />; */
+        })}
+
+        {/* <div className="to-buy-card">
           <img src={Finances} alt="onion" />
           <p>Onion</p>
           <p>!!!</p>
@@ -22,7 +46,7 @@ export default function Kitchen() {
           <img src={Finances} alt="onion" />
           <p>Onion</p>
           <p>!!!</p>
-        </div>
+        </div> */}
       </div>
       <h2>In house</h2>
       <h3>Groceries</h3>
---
+++ src/components/KitchenItemCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function KitchenItemCard(props) {
+  return (
+    <div className={props.type}>
+        <img src={props.imgsrc} alt={props.alttext} />
+        <p>{props.itemname}</p>
+    </div>
+  )
+}
