0abd69de040ce8cab0751996201a9b170d1e9be7
razvanpolitic <razvanpolitic@outlook.com>
2022-10-27 13:06:34 +0200

    CSS changes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,42 +14,36 @@ export default function Nav() {
         <div className="navlink-container">
           <NavLink className="navlink" to="/" >
             <img src={Home} alt="homeicon" className="nav-icon"/>
-            Home
           </NavLink>
         </div>
 
         <div className="navlink-container">
           <NavLink className="navlink" to="/kitchen">
             <img src={Kitchen} alt="homeicon" className="nav-icon"/>
-            Kitchen
           </NavLink>
         </div>
 
         <div className="navlink-container">
           <NavLink className="navlink" to="/laundry">
             <img src={Laundry} alt="homeicon" className="nav-icon"/>
-            Laundry
           </NavLink>
         </div>
 
         <div className="navlink-container">
           <NavLink className="navlink" to="/finances">
             <img src={Finances} alt="homeicon" className="nav-icon"/>
-            Finances
           </NavLink>
         </div>
 
         <div className="navlink-container">
           <NavLink className="navlink" to="/login">
             <img src={Home} alt="homeicon" className="nav-icon"/>
-            Log in
           </NavLink>
         </div>
       </nav>
       <div className="navlink-container">
         <NavLink className="navlink navlink-profile" to="/profile">
           <img src={Profile} alt="homeicon" className="nav-icon"/>
-          Profile
         </NavLink>
       </div>
     </div>
