5db4226db3854316cd22e9973d99e09216cf363e
razvanpolitic <razvanpolitic@outlook.com>
2022-10-28 14:46:45 +0200

    Deleted see more button

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -2,72 +2,63 @@ import React from 'react'
 
 export default function Transactions() {
   return (
-    <ul className='transactions'>
+    <div className='transactions'>
 
-      <li className='transaction'>
+      <div className='transaction'>
         <h3 className='transaction-name'>Kitchen tax paid</h3>
         <p className='transaction-date'>12.10.2022</p>
         <p className='transaction-amount'>-50</p>
         <hr />
-      </li>
+      </div>
 
-      <li className='transaction'>
+      <div className='transaction'>
         <h3 className='transaction-name'>Reciept 113</h3>
         <p className='transaction-date'>8.10.2022</p>
         <p className='transaction-amount'>+36</p>
         <hr />
-      </li>
+      </div>
 
-      <li className='transaction'>
+      <div className='transaction'>
         <h3 className='transaction-name'>Reciept 113</h3>
         <p className='transaction-date'>23.9.2022</p>
         <p className='transaction-amount'>+30</p>
         <hr />
-      </li>
+      </div>
 
-      <li className='transaction'>
+      <div className='transaction'>
         <h3 className='transaction-name'>Reciept 113</h3>
         <p className='transaction-date'>20.9.2022</p>
         <p className='transaction-amount'>+30</p>
         <hr />
-      </li>
+      </div>
 
-      {/* See more button */}
-
-      <input type="checkbox" id="more" name="view more" className="see-more_more-checkbox" /><label className="see-more_more-label" for="more"><span>See more
-      </span></label>
-      <input type="checkbox" id="more" name="view more" className="see-more_less-checkbox" /><label className="see-more_less-label" for="more"><span>Collapse
-      </span></label>
-
-      {/* The rest of transactions */}
-
-      <li className='transaction'>
+      <div className='transaction'>
         <h3 className='transaction-name'>Kitchen tax paid</h3>
         <p className='transaction-date'>12.10.2022</p>
         <p className='transaction-amount'>-50</p>
         <hr />
-      </li>
+      </div>
 
-      <li className='transaction'>
+      <div className='transaction'>
         <h3 className='transaction-name'>Reciept 113</h3>
         <p className='transaction-date'>8.10.2022</p>
         <p className='transaction-amount'>+36</p>
         <hr />
-      </li>
+      </div>
 
-      <li className='transaction'>
+      <div className='transaction'>
         <h3 className='transaction-name'>Reciept 113</h3>
         <p className='transaction-date'>23.9.2022</p>
         <p className='transaction-amount'>+30</p>
         <hr />
-      </li>
+      </div>
 
-      <li className='transaction'>
+      <div className='transaction'>
         <h3 className='transaction-name'>Reciept 113</h3>
         <p className='transaction-date'>20.9.2022</p>
         <p className='transaction-amount'>+30</p>
         <hr />
-      </li>
-    </ul>
+      </div>
+    </div>
   )
 }
