658ad3c103ee0e525386201ef726aa30210dff41
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-10-28 14:08:11 +0200

    Homepage cleanup, formatting

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,18 +5,13 @@ import Calendar from "../components/Calendar";
 import Nav from "../components/Nav";
 import AvailableMachines from "../components/AvailableMachines";
 import PostCard from "../components/PostCard.js";
-import { createUser, endpoint, getUser, getFromEndpoint } from '../utils/database_functions.js';
-export default function HomePage() {
+import { endpoint, getFromEndpoint } from "../utils/database_functions.js";
 
+export default function HomePage() {
   const [wms, setWms] = useState([]);
   const [users, setUsers] = useState([]);
-  /* const [items, setItems] = useState([]); */
   const [leases, setLeases] = useState([]);
 
-
-  /* Getting data from an endpoint */
-  
-  
   /* Getting washing machine data */
   async function getWms() {
     const url = `${endpoint}/WashingMachines.json`;
@@ -31,13 +26,6 @@ export default function HomePage() {
     setUsers(result);
   }
 
-  /* Getting items data */
- /*  async function getItems() {
-    const url = `${endpoint}/Items.json`;
-    let result = await getFromEndpoint(url);
-    setItems(result);
-  } */
-
   /* Getting leases data */
   async function getLeases() {
     const url = `${endpoint}/Leases.json`;
@@ -45,19 +33,16 @@ export default function HomePage() {
     setLeases(result);
   }
 
-
   useEffect(() => {
     getWms();
     getUsers();
-    //getItems();
     getLeases();
-   
   }, []);
 
   return (
     <>
       <article>
-{/*Sorry for the ugly way of hiding the washing machine display*/}
+        {/*Sorry for the ugly way of hiding the washing machine display*/}
         <div style={{ display: "none" }}>
           {wms.map((wm, i) => {
             // Return the element. Also pass key
@@ -65,11 +50,15 @@ export default function HomePage() {
           })}
         </div>
       </article>
-{/* 
+      {/* 
       <p>You are logged in as {currentuser.username}</p> */}
 
       <h1 className="header-title">HomePage</h1>
-      <h1> {JSON.parse(localStorage.getItem('loggedinname')) ? `Hello ${JSON.parse(localStorage.getItem('loggedinname'))}` : ""} </h1>
+      <h1>
+        {JSON.parse(localStorage.getItem("loggedinname"))
+          ? `Hello ${JSON.parse(localStorage.getItem("loggedinname"))}`
+          : ""}
+      </h1>
       <Balance />
       <Calendar />
       <AvailableMachines />
