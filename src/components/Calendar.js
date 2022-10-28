01638837ba92d743dd2d343fa6aa9ab861533209
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-10-28 14:15:26 +0200

    React fix

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,54 +2,54 @@ import React from "react";
 
 export default function Calendar() {
   return (
-    // <div className="calendar">
-    //   <div className="event">
-    //     <div className="date">
-    //       <div className="day">11</div>
-    //       <div className="month">OCT</div>
+    // <div classNameName="calendar">
+    //   <div classNameName="event">
+    //     <div classNameName="date">
+    //       <div classNameName="day">11</div>
+    //       <div classNameName="month">OCT</div>
     //     </div>
-    //     <div className="event-title">Kitchen Meeting</div>
+    //     <div classNameName="event-title">Kitchen Meeting</div>
     //   </div>
 
-    //   <div className="event">
-    //     <div className="date">
-    //       <div className="day">22</div>
-    //       <div className="month">OCT</div>
+    //   <div classNameName="event">
+    //     <div classNameName="date">
+    //       <div classNameName="day">22</div>
+    //       <div classNameName="month">OCT</div>
     //     </div>
-    //     <div className="event-title">Cleaning duty</div>
+    //     <div classNameName="event-title">Cleaning duty</div>
     //   </div>
 
-    //   <div className="event">
-    //     <div className="date">
-    //       <div className="day">30</div>
-    //       <div className="month">OCT</div>
+    //   <div classNameName="event">
+    //     <div classNameName="date">
+    //       <div classNameName="day">30</div>
+    //       <div classNameName="month">OCT</div>
     //     </div>
-    //     <div className="event-title">Kitchen tax due date</div>
+    //     <div classNameName="event-title">Kitchen tax due date</div>
     //   </div>
     // </div>
 
-    <div class="calendar-wrapper">
-      <ul class="calendar-flex calendar-cards">
+    <div className="calendar-wrapper">
+      <ul className="calendar-flex calendar-cards">
         <li>
-          <div class="date-nr">
-            <h1 class="nr">17</h1>
-            <h4 class="vertical-date">OCT</h4>
+          <div className="date-nr">
+            <h1 className="nr">17</h1>
+            <h4 className="vertical-date">OCT</h4>
           </div>
           <p>Kitchen meeting</p>
         </li>
 
         <li>
-          <div class="date-nr">
-            <h1 class="nr">21</h1>
-            <h4 class="vertical-date">NOV</h4>
+          <div className="date-nr">
+            <h1 className="nr">21</h1>
+            <h4 className="vertical-date">NOV</h4>
           </div>
           <p>Cleaning week</p>
         </li>
 
         <li>
-          <div class="date-nr">
-            <h1 class="nr">10</h1>
-            <h4 class="vertical-date">DEC</h4>
+          <div className="date-nr">
+            <h1 className="nr">10</h1>
+            <h4 className="vertical-date">DEC</h4>
           </div>
           <p>Tour de chambre</p>
         </li>
