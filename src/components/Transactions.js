401ec11c8d0d040aee4792483a4d2f1c8b7240ab
Giulia Ivan <eaagril@students.eaaa.dk>
2022-10-31 10:03:40 +0100

    grammar fix2

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -12,21 +12,21 @@ export default function Transactions() {
       </div>
 
       <div className='transaction'>
-        <h3 className='transaction-name'>Reciept 113</h3>
+        <h3 className='transaction-name'>Receipt 113</h3>
         <p className='transaction-date'>8.10.2022</p>
         <p className='transaction-amount'>+36</p>
         <hr />
       </div>
 
       <div className='transaction'>
-        <h3 className='transaction-name'>Reciept 113</h3>
+        <h3 className='transaction-name'>Receipt 113</h3>
         <p className='transaction-date'>23.9.2022</p>
         <p className='transaction-amount'>+30</p>
         <hr />
       </div>
 
       <div className='transaction'>
-        <h3 className='transaction-name'>Reciept 113</h3>
+        <h3 className='transaction-name'>Receipt 113</h3>
         <p className='transaction-date'>20.9.2022</p>
         <p className='transaction-amount'>+30</p>
         <hr />
