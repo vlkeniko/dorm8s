65b3bc35eab7e15cc07b9467598bf3e03ca1b846
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-10-31 10:03:28 +0100

    Added log out button

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,8 +1,16 @@
 import { React, useState, useEffect } from "react";
 import { endpoint, getFromEndpoint } from "../utils/database_functions";
 import profile from "../pictures/profile.png";
+import { forceHomePage } from "../pages/LoginPage";
 
 export default function Profile(props) {
+  let loggedinid = JSON.parse(localStorage.getItem("loggedinid"));
+
+  function handleLogout() {
+    localStorage.clear();
+    forceHomePage(1000);
+  }
+  
   return (
     <div className="profile-information">
       <div className="profile-image">
@@ -12,6 +20,7 @@ export default function Profile(props) {
         <h1>Name: {props.user.name}</h1>
         <h3>{props.lease.address}</h3>
         <h3>Lease#: {props.user.leasenum}</h3>
+        {loggedinid && loggedinid.length > 0 ? <button onClick={handleLogout}>Log out</button> : ""}
       </div>
     </div>
   );
