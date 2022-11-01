982651dc4c53b0d400167511b613047f965ee4dd
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-11-01 13:22:49 +0100

    Formatted

--- src/components/WM.js
+++ src/components/WM.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import Machine from '../components/WM/Machine'
-import { LaundryData } from '../laundry'
+import React from "react";
+import Machine from "../components/WM/Machine";
+import { LaundryData } from "../laundry";
 
 export default function WM() {
   return (
-    <div className='machines'>
+    <div className="machines">
       {/* We import the laundry data from a static JSON
       then iterate through each machine to make a component out of each one */}
       {LaundryData.map((machine, i) => {
-          return <Machine key={machine.type + machine.no} no={machine.no} type={machine.type} available={machine.available} RemainingTime={machine.RemainingTime} />;
-        })}
+        return (
+          <Machine
+            key={machine.type + machine.no}
+            no={machine.no}
+            type={machine.type}
+            available={machine.available}
+            RemainingTime={machine.RemainingTime}
+          />
+        );
+      })}
     </div>
-  )
+  );
 }
