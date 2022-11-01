e4e3f29f63358b2696763ea896e79dc95b7bec31
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-11-01 13:12:55 +0100

    - Laundry data now comes from JSON - Styling on machines page

--- src/components/WM.js
+++ src/components/WM.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Machine from '../components/WM/Machine'
+import { LaundryData } from '../laundry'
 
 export default function WM() {
   return (
     <div className='machines'>
-      <Machine />
-      <Machine />
-      <Machine />
-      <Machine />
+      {LaundryData.map((machine, i) => {
+          // Return the element. Also pass key
+          console.log(machine);
+          return <Machine no={machine.no} type={machine.type} available={machine.available} RemainingTime={machine.RemainingTime} />;
+        })}
     </div>
   )
 }
--- src/components/WM/Machine.js
+++ src/components/WM/Machine.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import { LaundryData } from "src\laundry";
 
-export default function Machine() {
+export default function Machine( props ) {
   return (
     <div className='machine'>
-        <div className='icon'>
+        <div className={props.available === "yes" ? "icon available" : "icon unavailable"}>
             <img src={require('../../pictures/laundry.png')} alt="Icon with a washing machine"/>
         </div>
         <p className='number'>
-            1 {/* db insert */}
+            {props.no/* db insert */}
         </p>
-        <p className='availability'>
-            Available {/* db insert */}
+        <p className='number'>
+            {props.available === "yes" ? "Available" : "Time left" /* db insert */}
         </p>
-        <button className='bookbutton'>
-            Book {/* available? "book" : getRemainingTime() */}
+        
+        <button className={props.available === "yes" ? "bookbutton" : "finance-button"} onClick={() => props.available === "no" ? alert("You can't book this machine yet!") : ""}>
+            {/* Book */} {props.available === "yes" ? "book" : props.RemainingTime}
         </button>
     </div>
   )
