e2d5284a550936e379913325c8632d1a9d6d9ee3
Giulia Ivan <eaagril@students.eaaa.dk>
2022-11-01 14:52:09 +0100

    finance buttons icons

--- src/components/FinanceButtons.jsx
+++ src/components/FinanceButtons.jsx
@@ -4,6 +4,7 @@ import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { db, storage } from "../firebaseConfig";
 import { useEffect } from "react";
 import budget from "../pictures/budget.png";
+import invoice from "../pictures/invoice.png";
 
 export default function FinanceButtons() {
   useEffect(() => {
@@ -98,16 +99,13 @@ export default function FinanceButtons() {
       <div className="finance-buttons">
         <a href="https://products.mobilepay.dk/box/invitation?id=2fN8cgSk&fbclid=IwAR22tSZaxhAWlDgbObzgwhKvU8TWUz0zqUpKliVvDQ9uvdFW8Qe9e8sstwQ">
           <button className="finance-button">
-            <img
-              src={budget}
-              alt="Icon of a washing machine"
-              id="budget"
-            />
+            <img src={budget} alt="Icon of a washing machine" id="budget" />
             Pay kitchen tax
           </button>
         </a>
         {/*Clicking this button will open the modal*/}
         <button className="finance-button" id="myBtn">
+          <img src={invoice} alt="Icon of a washing machine" id="budget" />
           Add receipt
         </button>
       </div>
