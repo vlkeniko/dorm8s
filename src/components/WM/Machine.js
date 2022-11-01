9c3287fd650e52e3cdf7a9c1a99963aa9275f149
KisLajos <64649246+KisLajos@users.noreply.github.com>
2022-11-01 13:22:17 +0100

    Formatted, cleanup, added comments

--- src/components/WM/Machine.js
+++ src/components/WM/Machine.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React from "react";
 
-export default function Machine( props ) {
+export default function Machine(props) {
   return (
-    <div className='machine'>
-        <div className={props.available === "yes" ? "icon available" : "icon unavailable"}>
-            <img src={require('../../pictures/laundry.png')} alt="Icon with a washing machine"/>
-        </div>
-        <p className='number'>
-            {props.no/* db insert */}
-        </p>
-        <p className='number'>
-            {props.available === "yes" ? "Available" : "Time left" /* db insert */}
-        </p>
-        
-        <button className={props.available === "yes" ? "bookbutton" : "finance-button"} onClick={() => props.available === "no" ? alert("You can't book this machine yet!") : ""}>
-            {/* Book */} {props.available === "yes" ? "book" : props.RemainingTime}
-        </button>
+    /* Depending on each machines availability we set various attributes on the components */
+    <div className="machine">
+      <div
+        className={
+          /* Here we set the icon color */
+          props.available === "yes" ? "icon available" : "icon unavailable"
+        }
+      >
+        <img
+          src={require("../../pictures/laundry.png")}
+          alt="Icon with a washing machine"
+        />
+      </div>
+      <p className="number">{props.no}</p>
+      <p className="availability">
+        {/* Here we set the text */}
+        {props.available === "yes" ? "Available" : "Time left"}
+      </p>
+
+      <button
+        /* Here we set the button's styling and an onClick() alert if needed */
+        className={props.available === "yes" ? "bookbutton" : "finance-button"}
+        onClick={() =>
+          props.available === "no"
+            ? alert("You can't book this machine yet!")
+            : ""
+        }
+      >
+        {/* Here we set the button text */}
+        {props.available === "yes" ? "book" : props.RemainingTime}
+      </button>
     </div>
-  )
+  );
 }
