18e11d1b724fb08f7faa74d4defbb57d48308aa2
Giulia Ivan <eaagril@students.eaaa.dk>
2022-11-01 10:00:03 +0100

    transactions
    changed the color to red for money paid

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -7,7 +7,7 @@ export default function Balance(props) {
     <>
       <div className="balance">
         <h3>Account balance</h3>
-        <h2 className="amount">(+{props.balance} kr.)</h2>
+        <h2 className="amount">+{props.balance} kr.</h2>
       </div>
     </>
   );
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -7,35 +7,35 @@ export default function Transactions() {
       <div className='transaction'>
         <h3 className='transaction-name'>Kitchen tax paid</h3>
         <p className='transaction-date'>12.10.2022</p>
-        <p className='transaction-amount'>-50</p>
+        <p className='transaction-amount' id='red-money'>-38.25 kr.</p>
         <hr />
       </div>
 
       <div className='transaction'>
         <h3 className='transaction-name'>Receipt 113</h3>
         <p className='transaction-date'>8.10.2022</p>
-        <p className='transaction-amount'>+36</p>
+        <p className='transaction-amount'>+36 kr.</p>
         <hr />
       </div>
 
       <div className='transaction'>
         <h3 className='transaction-name'>Receipt 113</h3>
         <p className='transaction-date'>23.9.2022</p>
-        <p className='transaction-amount'>+30</p>
+        <p className='transaction-amount'>+30 kr.</p>
         <hr />
       </div>
 
       <div className='transaction'>
         <h3 className='transaction-name'>Receipt 113</h3>
         <p className='transaction-date'>20.9.2022</p>
-        <p className='transaction-amount'>+30</p>
+        <p className='transaction-amount' id='red-money'>-127 kr.</p>
         <hr />
       </div>
 
       <div className='transaction'>
         <h3 className='transaction-name'>Kitchen tax paid</h3>
         <p className='transaction-date'>12.10.2022</p>
-        <p className='transaction-amount'>-50</p>
+        <p className='transaction-amount' id='red-money'>-94.17 kr.</p>
       </div>
 
     </div>
